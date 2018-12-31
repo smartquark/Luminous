b1e1013372806d1e84a4922052e0f76a19e68953
smartquark <smartquark@gmail.com>
2018-12-31 11:56:42 +0700

    run on iphone and upgraded xcode

--- Example/Luminous/AppDelegate.swift
+++ Example/Luminous/AppDelegate.swift
@@ -20,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 //        return true
 //    }
     
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    private func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
     }
 
